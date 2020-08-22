b2a684453653cd659a3cdb7d990be696e8754ef0
Johan Mancilla <johanmancilla99@gmail.com>
2020-08-21 22:28:13 -0500

    Plataforma Inicial

---
+++ Assets/Scripts/Camara.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    float mouseS = 90f;
+    float rotationOnX;
+    public Transform player;
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseS;
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * mouseS;
+
+        rotationOnX -= mouseY;
+        rotationOnX = Mathf.Clamp(rotationOnX, -90f, 90f);
+        transform.localEulerAngles = new Vector3(rotationOnX, 0f, 0f);
+
+        player.Rotate(Vector3.up * mouseX);
+
+
+    }
+}
---
+++ Assets/Scripts/MovimientoJugador.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoJugador : MonoBehaviour
+{
+    Rigidbody rig;
+
+    float moveForward;
+    float moveSide;
+    float moveUp;
+    bool inFloor;
+    public float speed = 5f;
+    public float jumpSpeed = 5f;
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveForward = Input.GetAxis("Vertical") * speed;
+        moveSide = Input.GetAxis("Horizontal") * speed;
+        moveUp = Input.GetAxis("Jump") * jumpSpeed;
+
+        rig.velocity = (transform.forward * moveForward) + (transform.right * moveSide) + (transform.up * rig.velocity.y);
+
+        if (inFloor)
+        {
+            rig.AddForce(transform.up * moveUp, ForceMode.VelocityChange);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            inFloor = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            inFloor = false;
+        }
+    }
+}
+
